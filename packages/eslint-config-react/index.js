1c2b5f55d456d00e234afe3b3993ce07068791cb
devrsi0n <devrsi0n@gmail.com>
2019-03-10 20:20:29 +0800

    feat: add eslint hooks plugin

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -6,8 +6,13 @@ module.exports = {
     '@devrsi0n/eslint-config-base',
     'eslint-config-prettier/react',
   ].map(require.resolve),
+  'plugins': [
+    'react-hooks'
+  ],
   rules: {
     strict: 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
     'react/destructuring-assignment': 'off'
   },
