9c7168ae67a3e241647227da7adea54a7cae8686
devrsi0n <devrsi0n@gmail.com>
2019-10-29 17:44:42 +0800

    fix(eslint-config-prettify-ts-base): rename directory eslint-config-ts-base -> ts-base, fix typescript import resolve

---
+++ packages/ts-base/index.js
@@ -0,0 +1,53 @@
+// 'use strict';
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint-config-prettify-base',
+
+    'plugin:@typescript-eslint/recommended',
+    // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/typescript',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['@typescript-eslint'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+      'babel-eslint': ['.js', '.jsx', '.json'],
+    },
+    'import/extensions': ['.ts', '.tsx', '.js', '.jsx'],
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<roo/>@types` directory even it doesn't contain any source code, like `@types/unist`
+        directory: process.cwd(),
+      },
+    },
+  },
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/camelcase': 'warn',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { varsIgnorePattern: '_' }],
+  },
+  // overrides: [
+  //   {
+  //     // enable the rule specifically for TypeScript files
+  //     files: ['*.ts', '*.tsx'],
+  //     rules: {
+  //       '@typescript-eslint/no-var-requires': ['off'],
+  //     },
+  //   },
+  // ],
+};
