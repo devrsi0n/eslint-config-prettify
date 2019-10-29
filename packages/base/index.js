b3dbe4969fd030ea72176861bcd394a586d725e9
devrsi0n <devrsi0n@gmail.com>
2019-10-29 17:42:17 +0800

    fix(eslint-config-prettify-base): rename directory eslint-config-base -> base, and using local rules instead of airbnb-base

---
+++ packages/base/index.js
@@ -0,0 +1,45 @@
+// 'use strict';
+
+const localRules = [
+  './rules/best-practices',
+  './rules/errors',
+  './rules/node',
+  './rules/style',
+  './rules/variables',
+  './rules/es6',
+  './rules/imports',
+  './rules/strict',
+].map(require.resolve);
+
+module.exports = {
+  parser: 'babel-eslint',
+  extends: [
+    ...localRules,
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2019,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+    },
+  },
+  env: {
+    browser: true,
+    worker: true,
+    jquery: true,
+    jest: true,
+    mocha: true,
+  },
+  plugins: ['prettier'],
+  rules: {
+    strict: 'off',
+    'no-plusplus': 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    ],
+  },
+};
